1e4fe63fff010c94facb5430888a6c9a40b982bd
JauniKappauni <social.jauni@gmail.com>
2025-11-22 13:49:15 +0100

    feat: add random number generation and game logic to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int generateRandomNumber()
+{
+    return rand() % 100;
+}
+
+int main()
+{
+    srand(time(0));
+    int randomNumber = generateRandomNumber();
+    int input;
+
+    do
+    {
+        cout << "Please enter a number: ";
+        cin >> input;
+
+        if (input < randomNumber)
+        {
+            cout << "Please enter a higher number. " << endl;
+        }
+        else if (input > randomNumber)
+        {
+            cout << "Please enter a lower number. " << endl;
+        }
+        else
+        {
+            cout << "You guessed the number " << endl;
+        }
+    } while (input != randomNumber);
 }
